0f52d2acca42afcbbe281e562b9f0b6b837e58b7
Alvaro <alvaro.martin.crespo.00@gmail.com>
2022-11-25 14:17:59 +0100

    update img portfolio

--- src/component/Page.jsx
+++ src/component/Page.jsx
@@ -8,6 +8,9 @@ import { useState } from 'react';
 //Creamos una page.jsx para separalo todo por componente y exportar
 //solo un componente a App.js
 function Page() {
+    const code1 = '<';
+    const code2 = ' />';
+    const name = 'AMC';
     /**
      * Creamos una array donde almacenamos el num de clics y una funcion
      * que va a aumentar los clics en uno o va a reinicar el
@@ -24,12 +27,17 @@ function Page() {
 
     return (
         <div className="containerPrincipal">
-            <div className="cocacola-contenedor">
-                <img
+            <div className="contenedor">
+                <p>
+                    <span className="blueSpan">{code1}</span>
+                    <span>{name}</span>
+                    <span className="greenSpan">{code2}</span>
+                </p>
+                {/* <img
                     className="cocacola-logo"
                     src={cocacolaLogo}
                     alt="logo-cocacola"
-                />
+                /> */}
             </div>
             <div className="contenedorAplicacion">
                 <Contador numClics={numClics} />
